5d280c83da77290279a203869576733b8f627bff
Martin Nzuki <iamnzuki@gmail.com>
2020-04-03 16:41:41 +0300

    Grid button upgrade

--- lib/screens/chamasoft/reports.dart
+++ lib/screens/chamasoft/reports.dart
@@ -1,8 +1,7 @@
 import 'package:chamasoft/screens/chamasoft/reports/contribution-statement.dart';
-import 'package:chamasoft/widgets/backgrounds.dart';
-import 'package:chamasoft/widgets/textstyles.dart';
+import 'package:chamasoft/widgets/buttons.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
 
 class ChamasoftReports extends StatefulWidget {
   @override
@@ -25,76 +24,30 @@ class _ChamasoftReportsState extends State<ChamasoftReports> {
     return OrientationBuilder(
       builder: (context, orientation) {
         return GridView.count(
-          padding: EdgeInsets.all(16.0),
+          padding: EdgeInsets.all(12.0),
           crossAxisCount: orientation == Orientation.portrait ? 2 : 4,
           children: List.generate(2, (index) {
             String title1 = "CONTRIBUTION";
             String title2 = "STATEMENT";
-            IconData icon = Feather.file_text;
+            IconData icon = LineAwesomeIcons.file_text;
             int statementFlag = 0;
             if (index == 1) {
               title1 = "FINE";
-              icon = Feather.file_minus;
+              icon = LineAwesomeIcons.file;
               statementFlag = 2;
             }
-
-            return GridItem(
-                title1: title1,
-                title2: title2,
-                icon: icon,
-                onTapped: () => Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              ContributionStatement(),
-                          settings: RouteSettings(arguments: statementFlag)),
-                    ));
+            return gridButton(
+              context: context,
+              icon: icon,
+              title: title1,
+              subtitle: title2,
+              color: Colors.blueGrey[400],
+              isHighlighted: false,
+              action: () => Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => ContributionStatement(), settings: RouteSettings(arguments: statementFlag)),),
+            );
           }),
         );
       },
-    );
-  }
-}
-
-class GridItem extends StatelessWidget {
-  const GridItem(
-      {Key key,
-      @required this.title1,
-      @required this.title2,
-      @required this.icon,
-      @required this.onTapped})
-      : super(key: key);
-
-  final String title1;
-  final String title2;
-  final IconData icon;
-  final Function onTapped;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTapped,
-      child: Container(
-        margin: EdgeInsets.all(16),
-        height: 150,
-        decoration: cardDecoration(
-            gradient: plainCardGradient(context), context: context),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              icon,
-              size: 35.0,
-              color: Colors.blue,
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            subtitle1(
-                text: title1, color: Colors.blue, align: TextAlign.center),
-            subtitle2(text: title2, color: Colors.blue, align: TextAlign.center)
-          ],
-        ),
-      ),
     );
   }
 }
