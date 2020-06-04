dd091b6376609f0ceaa5aed8c04a1048eec4f214
Edwin Kapkei <dev.kapkei@gmail.com>
2020-06-04 12:46:18 +0300

    updates on my-groups.dart

--- lib/screens/my-groups.dart
+++ lib/screens/my-groups.dart
@@ -37,79 +37,84 @@ class _MyGroupsState extends State<MyGroups> {
         alignment: Alignment.center,
         decoration: primaryGradient(context),
         height: MediaQuery.of(context).size.height,
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              heading1(
-                  text: "My Groups",
-                  color: Theme.of(context).textSelectionHandleColor),
-              subtitle1(
-                  text: "All groups I belong to",
-                  color: Theme.of(context).textSelectionHandleColor),
-              SizedBox(
-                height: 32,
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-                child: Image(
-                  image: AssetImage('assets/no-user.png'),
-                  height: 80.0,
+        child: RefreshIndicator(
+          displacement: 40,
+          color: primaryColor,
+          onRefresh: () {}, //TODO implement refresh list
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.all(40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                heading1(
+                    text: "My Groups",
+                    color: Theme.of(context).textSelectionHandleColor),
+                subtitle1(
+                    text: "All groups I belong to",
+                    color: Theme.of(context).textSelectionHandleColor),
+                SizedBox(
+                  height: 32,
                 ),
-              ),
-              heading2(
-                  text: auth.userName,
-                  color: Theme.of(context).textSelectionHandleColor),
-              subtitle1(
-                  text: auth.phoneNumber, //auth.phoneNumber,
-                  color: Theme.of(context)
-                      .textSelectionHandleColor
-                      .withOpacity(0.6)),
-              SizedBox(
-                height: 20,
-              ),
-              groupInfoButton(
-                context: context,
-                leadingIcon: LineAwesomeIcons.plus,
-                trailingIcon: LineAwesomeIcons.angle_right,
-                hideTrailingIcon: true,
-                backgroundColor: primaryColor.withOpacity(0.2),
-                title: "ADD NEW GROUP",
-                subtitle: "Chairperson",
-                textColor: primaryColor,
-                borderColor: primaryColor,
-                action: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => CreateGroup(),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                  child: Image(
+                    image: AssetImage('assets/no-user.png'),
+                    height: 80.0,
                   ),
                 ),
-              ),
-              ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: auth.groups.length,
-                  itemBuilder: (context, index) {
-                    InvestmentGroup groupModel = auth.groups[index];
-                    return groupInfoButton(
-                      context: context,
-                      leadingIcon: LineAwesomeIcons.group,
-                      trailingIcon: LineAwesomeIcons.angle_right,
-                      backgroundColor: primaryColor.withOpacity(0.2),
-                      title: "${groupModel.name}",
-                      subtitle: "${groupModel.size} Members",
-                      description: "${groupModel.role}",
-                      textColor: Colors.blueGrey,
-                      borderColor: Colors.blueGrey.withOpacity(0.2),
-                      action: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (BuildContext context) =>
-                              ChamasoftDashboard(),
+                heading2(
+                    text: auth.userName,
+                    color: Theme.of(context).textSelectionHandleColor),
+                subtitle1(
+                    text: auth.phoneNumber, //auth.phoneNumber,
+                    color: Theme.of(context)
+                        .textSelectionHandleColor
+                        .withOpacity(0.6)),
+                SizedBox(
+                  height: 20,
+                ),
+                groupInfoButton(
+                  context: context,
+                  leadingIcon: LineAwesomeIcons.plus,
+                  trailingIcon: LineAwesomeIcons.angle_right,
+                  hideTrailingIcon: true,
+                  backgroundColor: primaryColor.withOpacity(0.2),
+                  title: "ADD NEW GROUP",
+                  subtitle: "Chairperson",
+                  textColor: primaryColor,
+                  borderColor: primaryColor,
+                  action: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => CreateGroup(),
+                    ),
+                  ),
+                ),
+                ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: auth.groups.length,
+                    itemBuilder: (context, index) {
+                      InvestmentGroup groupModel = auth.groups[index];
+                      return groupInfoButton(
+                        context: context,
+                        leadingIcon: LineAwesomeIcons.group,
+                        trailingIcon: LineAwesomeIcons.angle_right,
+                        backgroundColor: primaryColor.withOpacity(0.2),
+                        title: "${groupModel.name}",
+                        subtitle: "${groupModel.size} Members",
+                        description: "${groupModel.role}",
+                        textColor: Colors.blueGrey,
+                        borderColor: Colors.blueGrey.withOpacity(0.2),
+                        action: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                ChamasoftDashboard(),
+                          ),
                         ),
-                      ),
-                    );
-                  }),
+                      );
+                    }),
 //              SizedBox(
 //                height: 32,
 //              ),
@@ -120,7 +125,8 @@ class _MyGroupsState extends State<MyGroups> {
 //                },
 //                child: Text('Logout'),
 //              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
