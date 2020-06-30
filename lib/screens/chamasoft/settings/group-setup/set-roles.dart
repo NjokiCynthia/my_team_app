8e11cd72446f57caa239e93e670fbfd422710ca7
Edwin Kapkei <dev.kapkei@gmail.com>
2020-06-30 11:22:07 +0300

    ongoing updates on setting group roles

--- lib/screens/chamasoft/settings/group-setup/set-roles.dart
+++ lib/screens/chamasoft/settings/group-setup/set-roles.dart
@@ -4,6 +4,7 @@ import 'package:chamasoft/providers/groups.dart';
 import 'package:chamasoft/screens/chamasoft/models/custom-contact.dart';
 import 'package:chamasoft/screens/chamasoft/models/group-model.dart';
 import 'package:chamasoft/widgets/appbars.dart';
+import 'package:chamasoft/widgets/buttons.dart';
 import 'package:chamasoft/widgets/textstyles.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
@@ -24,6 +25,7 @@ class _SetMemberRolesState extends State<SetMemberRoles> {
   GroupRoles memberRole = GroupRoles(roleId: "0", roleName: "Member");
   List<CustomContact> selectedContacts = [];
   Map<String, int> roleStatus = {};
+  GlobalKey _dropdownButtonKey;
 
   void _updateRoleStatus(GroupRoles currentRole, GroupRoles newRole) {
     if ("0" == currentRole.roleId && "0" != newRole.roleId) {
@@ -49,17 +51,37 @@ class _SetMemberRolesState extends State<SetMemberRoles> {
   }
 
   void _cleanRolesList() {
-    setState(() {
-      roleStatus.forEach((key, value) {
-        if (value == 1) {
-          roles.forEach((element) {
-            if (identical(key, element)) {
-              tempRoles.add(element);
-            }
-          });
-        }
-      });
+    roleStatus.forEach((key, value) {
+      if (value == 1) {
+        roles.forEach((element) {
+          if (identical(key, element)) {
+            tempRoles.add(element);
+          }
+        });
+      }
     });
+  }
+
+  void _showDropDown() {
+/*    DropdownButton(
+      hint: Text("Set Role"),
+      value: tempRoles.indexOf(customContact.role) != -1 ? tempRoles[tempRoles.indexOf(customContact.role)] : tempRoles[0],
+      onChanged: (GroupRoles role) {
+        setState(() {
+          customContact.role = role;
+          //_updateRoleStatus(, role);
+        });
+      },
+      items: tempRoles.map((GroupRoles role) {
+        return DropdownMenuItem<GroupRoles>(
+          value: role,
+          child: Text(
+            role.roleName,
+            style: TextStyle(color: Colors.black),
+          ),
+        );
+      }).toList(),
+    );*/
   }
 
   @override
@@ -71,6 +93,25 @@ class _SetMemberRolesState extends State<SetMemberRoles> {
   @override
   Widget build(BuildContext context) {
     roles = Provider.of<Groups>(context).getCurrentGroup().groupRoles;
+
+    final dropDown = DropdownButton(
+      key: _dropdownButtonKey,
+      onChanged: (GroupRoles role) {
+        setState(() {
+          //customContact.role = role;
+          //_updateRoleStatus(, role);
+        });
+      },
+      items: tempRoles.map((GroupRoles role) {
+        return DropdownMenuItem<GroupRoles>(
+          value: role,
+          child: Text(
+            role.roleName,
+            style: TextStyle(color: Colors.black),
+          ),
+        );
+      }).toList(),
+    );
 
     return Scaffold(
       appBar: secondaryPageAppbar(
@@ -111,6 +152,7 @@ class _SetMemberRolesState extends State<SetMemberRoles> {
                         width: 10,
                       ),
                       Expanded(
+                        flex: 3,
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
@@ -121,24 +163,16 @@ class _SetMemberRolesState extends State<SetMemberRoles> {
                           ],
                         ),
                       ),
-                      DropdownButton(
-                        hint: Text("Set Role"),
-                        value: tempRoles.indexOf(customContact.role) != -1 ? tempRoles[tempRoles.indexOf(customContact.role)] : tempRoles[0],
-                        onChanged: (GroupRoles role) {
-                          setState(() {
-                            customContact.role = role;
-                            //_updateRoleStatus(, role);
-                          });
-                        },
-                        items: tempRoles.map((GroupRoles role) {
-                          return DropdownMenuItem<GroupRoles>(
-                            value: role,
-                            child: Text(
-                              role.roleName,
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          );
-                        }).toList(),
+                      Expanded(
+                        flex: 2,
+                        child: smallBadgeButton(
+                          backgroundColor: Colors.blueGrey.withOpacity(0.2),
+                          textColor: Colors.blueGrey,
+                          text: tempRoles[0].roleName,
+                          action: () {},
+                          buttonHeight: 24.0,
+                          textSize: 12.0,
+                        ),
                       ),
                     ],
                   ),
