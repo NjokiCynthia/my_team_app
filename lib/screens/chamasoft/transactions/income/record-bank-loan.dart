f86c642d505a21e5251b6e6e785866e118ba8711
githaigageoffrey <githaiga.geoffrey@gmail.com>
2020-07-07 14:36:35 +0300

    error in recording bank loan

--- lib/screens/chamasoft/transactions/income/record-bank-loan.dart
+++ lib/screens/chamasoft/transactions/income/record-bank-loan.dart
@@ -26,13 +26,14 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
   DateTime loanToDate = DateTime.now();
   DateTime now = DateTime.now();
   bool _isInit = true;
-  Map <String,dynamic> formLoadData = {},_formData;
+  Map<String, dynamic> formLoadData = {}, _formData={};
   var accountId;
   final _formKey = new GlobalKey<FormState>();
-  bool  _isLoading = false,_isFormInputEnabled = true;
+  bool _isLoading = false, _isFormInputEnabled = true;
   static final int epochTime = DateTime.now().toUtc().millisecondsSinceEpoch;
   String requestId = ((epochTime.toDouble() / 1000).toStringAsFixed(0));
   String loanDescription;
+  double amountLoaned, totalLoanAmountPayable, loanBalance;
 
   void _scrollListener() {
     double newElevation = _scrollController.offset > 1 ? appBarElevation : 0;
@@ -68,23 +69,23 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
   Future<void> _fetchDefaultValues(BuildContext context) async {
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       showDialog<String>(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context){
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      );
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          });
     });
-    formLoadData = await Provider.of<Groups>(context,listen: false).loadInitialFormData(acc:true); 
+    formLoadData = await Provider.of<Groups>(context, listen: false)
+        .loadInitialFormData(acc: true);
     setState(() {
       _isInit = false;
     });
-    Navigator.of(context,rootNavigator: true).pop();
+    Navigator.of(context, rootNavigator: true).pop();
   }
 
-  void _submit(BuildContext context) async{
+  void _submit(BuildContext context) async {
     if (!_formKey.currentState.validate()) {
       return;
     }
@@ -93,10 +94,18 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
       _isFormInputEnabled = false;
     });
     _formKey.currentState.save();
-    _formData["request_id"] = requestId;
+    //_formData["request_id"] = requestId;
     _formData["description"] = loanDescription;
-    try{
-      await Provider.of<Groups>(context, listen: false).recordBankLoanIncome(_formData);
+    _formData["amount_loaned"] = amountLoaned;
+    _formData["total_loan_amount_payable"] = totalLoanAmountPayable;
+    _formData["loan_balance"] = loanBalance;
+    _formData["loan_start_date"] = loanFromDate.toString();
+    _formData["loan_end_date"] = loanToDate.toString();
+    _formData["account_id"] = accountId;
+    print(_formData);
+    try {
+      await Provider.of<Groups>(context, listen: false)
+          .recordBankLoanIncome(_formData);
       Navigator.of(context).pop();
     } on CustomException catch (error) {
       StatusHandler().handleStatus(
@@ -111,7 +120,6 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
         _isFormInputEnabled = true;
       });
     }
-
   }
 
   @override
@@ -133,7 +141,12 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
           controller: _scrollController,
           child: Column(
             children: <Widget>[
-              toolTip(context: context,title: "Manually record loans received from financial institutions",message: "",),
+              toolTip(
+                context: context,
+                title:
+                    "Manually record loans received from financial institutions",
+                message: "",
+              ),
               Padding(
                 padding: inputPagePadding,
                 child: Form(
@@ -145,11 +158,10 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
                       simpleTextInputField(
                           context: context,
                           labelText: "Bank loan description",
-                          validator: (value){
-                            print("Length is here");
-                            if(value==""||value==null){
+                          validator: (value) {
+                            if (value == "" || value == null) {
                               return "This field is required";
-                            }else if(value.toString().length < 10){
+                            } else if (value.toString().length < 8) {
                               return "Description is too short";
                             }
                             return null;
@@ -161,50 +173,67 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
                           }),
                       amountTextInputField(
                           context: context,
-                          validator: (value){
-                            if(value==""||value==null){
-                              return "This field is required";
+                          validator: (value) {
+                            if (value == null || value == "") {
+                              print("Empty value2");
+                              return "Field is required";
                             }
                             return null;
                           },
                           labelText: "Total amount received",
-                          onChanged: (value) {}),
+                          onChanged: (value) {
+                            setState(() {
+                              amountLoaned = value;
+                            });
+                          }),
                       amountTextInputField(
                           context: context,
-                          validator: (value){
-                            if(value==""||value==null){
-                              return "This field is required";
+                          validator: (value) {
+                            if (value == null || value == "") {
+                              print("Empty value");
+                              return "Field is required";
                             }
                             return null;
                           },
                           labelText: "Total amount payable",
-                          onChanged: (value) {}),
+                          onChanged: (value) {
+                            setState(() {
+                              totalLoanAmountPayable = value;
+                            });
+                          }),
                       amountTextInputField(
                           context: context,
-                          validator: (value){
-                            if(value==""||value==null){
-                              return "This field is required";
+                          validator: (value) {
+                            print("Empty value3");
+                            if (value == null || value == "") {
+                              return "Field is required";
                             }
                             return null;
                           },
                           labelText: "Total loan balance as at date",
-                          onChanged: (value) {}),
+                          onChanged: (value) {
+                            setState(() {
+                              loanBalance = value;
+                            });
+                          }),
                       Row(
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: <Widget>[
                           Expanded(
-                            flex: 1,
-                            child:DatePicker(
-                              labelText: 'Select Deposit Date',
-                              lastDate: DateTime.now(),
-                              selectedDate: loanFromDate == null ? new DateTime(now.year, now.month, now.day - 1, 6, 30) : loanFromDate,
-                              selectDate: (selectedDate) {
-                                setState(() {
-                                  loanFromDate = selectedDate;
-                                });
-                              },
-                            ) 
-                          ),
+                              flex: 1,
+                              child: DatePicker(
+                                labelText: 'Select Deposit Date',
+                                lastDate: DateTime.now(),
+                                selectedDate: loanFromDate == null
+                                    ? new DateTime(
+                                        now.year, now.month, now.day - 1, 6, 30)
+                                    : loanFromDate,
+                                selectDate: (selectedDate) {
+                                  setState(() {
+                                    loanFromDate = selectedDate;
+                                  });
+                                },
+                              )),
                           SizedBox(
                             width: 5,
                           ),
@@ -212,9 +241,15 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
                             flex: 1,
                             child: DatePicker(
                               labelText: 'Loan To',
-                              firstDate: loanFromDate == null ? new DateTime(now.year, now.month, now.day - 1, 6, 30) : loanFromDate,
+                              firstDate: loanFromDate == null
+                                  ? new DateTime(
+                                      now.year, now.month, now.day - 1, 6, 30)
+                                  : loanFromDate,
                               lastDate: DateTime(2037),
-                              selectedDate: loanToDate == null ? new DateTime(now.year, now.month, now.day - 1, 6, 30) : loanToDate,
+                              selectedDate: loanToDate == null
+                                  ? new DateTime(
+                                      now.year, now.month, now.day - 1, 6, 30)
+                                  : loanToDate,
                               selectDate: (selectedDate) {
                                 setState(() {
                                   loanToDate = selectedDate;
@@ -226,15 +261,17 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
                       ),
                       CustomDropDownButton(
                         labelText: "Account loan deposited to",
-                        listItems: formLoadData.containsKey("accountOptions")?formLoadData["accountOptions"]:[],
+                        listItems: formLoadData.containsKey("accountOptions")
+                            ? formLoadData["accountOptions"]
+                            : [],
                         selectedItem: accountId,
                         onChanged: (value) {
                           setState(() {
                             accountId = value;
                           });
                         },
-                        validator: (value){
-                          if(value==""||value==null){
+                        validator: (value) {
+                          if (value == "" || value == null) {
                             return "This field is required";
                           }
                           return null;
@@ -244,17 +281,17 @@ class _RecordBankLoanState extends State<RecordBankLoan> {
                         height: 10,
                       ),
                       _isLoading
-                      ? Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Center(
-                            child: CircularProgressIndicator()
-                          ),
-                      ):
-                      defaultButton(
-                        context: context,
-                        text: "SAVE",
-                        onPressed: ()=>_submit(context),
-                      )
+                          ? Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Center(child: CircularProgressIndicator()),
+                            )
+                          : defaultButton(
+                              context: context,
+                              text: "SAVE",
+                              onPressed: () {
+                                print("this is clicked");
+                                _submit(context);
+                              })
                     ],
                   ),
                 ),
