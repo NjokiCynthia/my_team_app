cbc941681a60502b3dd162639f07a865cfc993dd
Edwin Kapkei <dev.kapkei@gmail.com>
2020-07-24 14:00:46 +0300

    ui updates on contribution list

--- lib/screens/chamasoft/settings/list-contributions.dart
+++ lib/screens/chamasoft/settings/list-contributions.dart
@@ -1,10 +1,12 @@
 import 'package:chamasoft/providers/groups.dart';
+import 'package:chamasoft/utilities/common.dart';
 import 'package:chamasoft/utilities/custom-helper.dart';
 import 'package:chamasoft/utilities/status-handler.dart';
 import 'package:chamasoft/utilities/theme.dart';
 import 'package:chamasoft/widgets/appbars.dart';
 import 'package:chamasoft/widgets/backgrounds.dart';
 import 'package:chamasoft/widgets/buttons.dart';
+import 'package:chamasoft/widgets/textstyles.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
 import 'package:provider/provider.dart';
@@ -17,16 +19,20 @@ class ListContributions extends StatefulWidget {
 }
 
 class _ListContributionsState extends State<ListContributions> {
+  String _groupCurrency = "Ksh";
+
   Future<void> _getContributionSettings(BuildContext context, String contributionId) async {
     try {
       final response = await Provider.of<Groups>(context, listen: false).getContributionDetails(contributionId);
       print(response);
       Navigator.pop(context);
+      Navigator.pop(context); // pop bottom sheet
       Navigator.of(context).push(MaterialPageRoute(
         builder: (context) => CreateContribution(isEditMode: true, contributionDetails: response),
       ));
     } on CustomException catch (error) {
       Navigator.pop(context);
+      Navigator.pop(context); // pop bottom sheet
       StatusHandler().handleStatus(
           context: context,
           error: error,
@@ -36,9 +42,78 @@ class _ListContributionsState extends State<ListContributions> {
     }
   }
 
+  void _showActions(BuildContext context, Contribution contribution) {
+    showModalBottomSheet<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Material(
+                  color: Theme.of(context).backgroundColor,
+                  child: InkWell(
+                    splashColor: Colors.blueGrey.withOpacity(0.2),
+                    onTap: () async {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          });
+                      await _getContributionSettings(context, contribution.id);
+                    },
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.edit,
+                        color: Colors.blueGrey,
+                      ),
+                      title: customTitle(
+                          text: "Edit Contribution",
+                          fontWeight: FontWeight.w600,
+                          textAlign: TextAlign.start,
+                          color: Theme.of(context).textSelectionHandleColor),
+                    ),
+                  ),
+                ),
+//                Material(
+//                  color: Theme.of(context).backgroundColor,
+//                  child: InkWell(
+//                    onTap: () {},
+//                    splashColor: Colors.blueGrey.withOpacity(0.2),
+//                    child: ListTile(
+//                      leading: Icon(
+//                        Icons.delete,
+//                        color: Colors.red.withOpacity(0.7),
+//                      ),
+//                      title: customTitle(
+//                          text: "Delete Contribution",
+//                          fontWeight: FontWeight.w600,
+//                          textAlign: TextAlign.start,
+//                          color: Theme.of(context).textSelectionHandleColor),
+//                      onTap: () {
+//                        Navigator.pop(context);
+//                      },
+//                    ),
+//                  ),
+//                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     super.initState();
+    _groupCurrency = Provider.of<Groups>(context, listen: false).getCurrentGroup().groupCurrency;
   }
 
   @override
@@ -49,7 +124,6 @@ class _ListContributionsState extends State<ListContributions> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
       appBar: secondaryPageAppbar(
         context: context,
         action: () => Navigator.of(context).pop(),
@@ -68,155 +142,102 @@ class _ListContributionsState extends State<ListContributions> {
           ));
         },
       ),
-      body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: primaryGradient(context),
-          child: Consumer<Groups>(builder: (context, groupData, child) {
-            return ListView.separated(
-              padding: EdgeInsets.only(bottom: 100.0, top: 10.0),
-              itemCount: groupData.contributions.length,
-              itemBuilder: (context, index) {
-                Contribution contribution = groupData.contributions[index];
-                return Card(
-                  color: Colors.white,
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(16.0),
-                    dense: true,
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 3,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  Icon(
-                                    Icons.account_balance_wallet,
-                                    color: Colors.blueGrey,
-                                  ),
-                                  SizedBox(width: 10.0),
-                                  Column(
+      //backgroundColor: Theme.of(context).backgroundColor,
+      backgroundColor: Colors.transparent,
+      body: Builder(
+        builder: (BuildContext context) {
+          return Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: primaryGradient(context),
+              child: Consumer<Groups>(builder: (context, groupData, child) {
+                return ListView.separated(
+                  padding: EdgeInsets.only(bottom: 50.0, top: 10.0),
+                  itemCount: groupData.contributions.length,
+                  itemBuilder: (context, index) {
+                    Contribution contribution = groupData.contributions[index];
+                    return ListTile(
+                      contentPadding: EdgeInsets.all(12.0),
+                      dense: true,
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            flex: 3,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.label,
+                                  color: Colors.blueGrey,
+                                ),
+                                SizedBox(width: 10.0),
+                                Expanded(
+                                  child: Column(
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: <Widget>[
-                                      Text(
-                                        '${contribution.name}',
-                                        style: TextStyle(
-                                          color: Theme.of(context).textSelectionHandleColor,
-                                          fontWeight: FontWeight.w800,
-                                          fontSize: 18.0,
-                                        ),
+                                      heading2(
+                                        text: '${contribution.name}',
+                                        textAlign: TextAlign.start,
+                                        color: Theme.of(context).textSelectionHandleColor,
                                       ),
-                                      Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Row(
-                                            children: <Widget>[
-                                              Text(
-                                                'Contribution Type: ',
-                                                style: TextStyle(
-                                                  fontWeight: FontWeight.w600,
-                                                  color: Theme.of(context).textSelectionHandleColor.withOpacity(0.5),
-                                                  fontSize: 12.0,
-                                                ),
-                                              ),
-                                              Text(
-                                                '${contribution.type}',
-                                                style: TextStyle(
-                                                  fontWeight: FontWeight.w900,
-                                                  color: Theme.of(context).textSelectionHandleColor.withOpacity(0.5),
-                                                  fontSize: 12.0,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                          Row(
-                                            children: <Widget>[
-                                              Text(
-                                                'Frequency: ',
-                                                style: TextStyle(
-                                                  fontWeight: FontWeight.w600,
-                                                  color: Theme.of(context).textSelectionHandleColor.withOpacity(0.5),
-                                                  fontSize: 12.0,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                          Text(
-                                            '${contribution.frequency}',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w900,
-                                              color: Theme.of(context).textSelectionHandleColor.withOpacity(0.5),
-                                              fontSize: 12.0,
-                                            ),
-                                          ),
-                                          Text(
-                                            'Amount: ${contribution.amount}',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w900,
-                                              color: Theme.of(context).textSelectionHandleColor.withOpacity(0.5),
-                                              fontSize: 18.0,
-                                            ),
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  Column(
-                                    children: <Widget>[
-                                      circleIconButton(
-                                        icon: Icons.edit,
-                                        backgroundColor: primaryColor.withOpacity(.3),
-                                        color: primaryColor,
-                                        iconSize: 18.0,
-                                        padding: 0.0,
-                                        onPressed: () async {
-                                          showDialog(
-                                              context: context,
-                                              builder: (BuildContext context) {
-                                                return Center(
-                                                  child: CircularProgressIndicator(),
-                                                );
-                                              });
-                                          await _getContributionSettings(context, contribution.id);
-                                        },
+                                      customTitleWithWrap(
+                                        text: 'Contribution Type: ${contribution.type}',
+                                        color: Theme.of(context).textSelectionHandleColor,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 12.0,
+                                        textAlign: TextAlign.start,
+                                      ),
+                                      customTitleWithWrap(
+                                        text: 'Frequency: ${contribution.frequency}',
+                                        color: Theme.of(context).textSelectionHandleColor,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 12.0,
+                                        textAlign: TextAlign.start,
+                                      ),
+                                      customTitle(
+                                        text: '$_groupCurrency ${currencyFormat.format(double.tryParse(contribution.amount) ?? 0)}',
+                                        color: Theme.of(context).textSelectionHandleColor,
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 12.0,
+                                        textAlign: TextAlign.start,
                                       ),
                                     ],
                                   ),
-                                ],
-                              ),
-                            ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          SizedBox(
+                            height: 40,
+                            width: 40,
+                            child: circleIconButton(
+                              icon: Icons.edit,
+                              backgroundColor: primaryColor.withOpacity(.3),
+                              color: primaryColor,
+                              iconSize: 16.0,
+                              padding: 0.0,
+                              onPressed: () => _showActions(context, contribution),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      color: Theme.of(context).dividerColor,
+                      height: 6.0,
+                    );
+                  },
                 );
-              },
-              separatorBuilder: (context, index) {
-                return Divider(
-                  color: Theme.of(context).dividerColor,
-                  height: 6.0,
-                );
-              },
-            );
-          })),
+              }));
+        },
+      ),
     );
   }
 }
