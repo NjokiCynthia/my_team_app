27e0427b7889d8005420c2cc505eae93dac18463
Martin Nzuki <iamnzuki@gmail.com>
2020-07-30 10:54:29 +0300

    e-wallet section UI

--- lib/screens/chamasoft/transactions.dart
+++ lib/screens/chamasoft/transactions.dart
@@ -1,6 +1,8 @@
 import 'package:chamasoft/screens/chamasoft/models/transaction-menu.dart';
 import 'package:chamasoft/screens/chamasoft/transactions/transaction-menu-details.dart';
 import 'package:chamasoft/screens/chamasoft/dashboard.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/wallet/review-withdrawal-requests.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/wallet/withdrawal-purpose.dart';
 import 'package:chamasoft/utilities/svg-icons.dart';
 import 'package:chamasoft/widgets/buttons.dart';
 import 'package:flutter/material.dart';
@@ -52,31 +54,96 @@ class _ChamasoftTransactionsState extends State<ChamasoftTransactions> {
       TransactionMenuSvg("INVOICING & TRANSFER", customIcons['transaction']),
     ];
 
+    List<Widget> eWalletOptions = [
+      SizedBox(
+        width: 16.0,
+      ),
+      Container(
+        width: 160.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['wallet'],
+          title: 'CREATE WITHDRAWAL REQUEST',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => WithdrawalPurpose(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 160.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['couple'],
+          title: 'REVIEW WITHDRAWAL REQUESTS',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => ReviewWithdrawalRequests(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+    ];
+
     return new WillPopScope(
       onWillPop: _onWillPop ,
-      child: OrientationBuilder(
-        builder: (context, orientation) {
-          return GridView.count(
-            controller: _scrollController,
-            padding: EdgeInsets.fromLTRB(6, 6, 6, 6),
-            crossAxisCount: orientation == Orientation.portrait ? 2 : 4,
-            children: List.generate(list.length, (index) {
-              return svgGridButton(
-                context: context,
-                icon: list[index].icon,
-                title: list[index].title,
-                color: (index == 0) ? Colors.white : Colors.blue[400],
-                isHighlighted: (index == 0) ? true : false,
-                action: () => Navigator.of(context).push(MaterialPageRoute(
-                    builder: (BuildContext context) => TransactionMenuDetails(),
-                    settings: RouteSettings(arguments: index)
-                  )
+      child: SafeArea(
+        child: SingleChildScrollView(
+          // controller: _scrollController,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 10.0, 16.0, 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "E-Wallet",
+                      style: TextStyle(
+                        color: Colors.blueGrey[400],
+                        fontFamily: 'SegoeUI',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    // IconButton(
+                    //     icon: Icon(
+                    //       Feather.more_horizontal,
+                    //       color: Colors.blueGrey,
+                    //     ),
+                    //     onPressed: () {})
+                  ],
                 ),
-                margin: 12
-              );
-            }),
-          );
-        },
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                child: Container(
+                  height: 180.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(top: 5.0, bottom: 10.0),
+                    physics: BouncingScrollPhysics(),
+                    children: eWalletOptions,
+                  ),
+                ),
+              ),
+              
+            ]
+          )
+        )
       )
     );
   }
