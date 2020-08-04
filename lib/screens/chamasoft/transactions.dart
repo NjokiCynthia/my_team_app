aa2678ac2d7539a0fa53d3da9bb72d7553d0e03a
Martin Nzuki <iamnzuki@gmail.com>
2020-08-04 15:39:25 +0300

    Grouping Transactions

--- lib/screens/chamasoft/transactions.dart
+++ lib/screens/chamasoft/transactions.dart
@@ -1,11 +1,22 @@
-import 'package:chamasoft/screens/chamasoft/models/transaction-menu.dart';
-import 'package:chamasoft/screens/chamasoft/transactions/transaction-menu-details.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/expenditure/bank-loan-repayments.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/expenditure/record-contribution-refund.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/expenditure/record-expense.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/income/record-bank-loan.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/income/record-contribution-payment.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/income/record-fine-payment.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/income/record-income.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/income/record-miscellaneous-payment.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/invoicing-and-transfer/account-to-account-transfer.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/invoicing-and-transfer/contribution-transfer.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/invoicing-and-transfer/create-invoice.dart';
+import 'package:chamasoft/screens/chamasoft/transactions/invoicing-and-transfer/fine-member.dart';
 import 'package:chamasoft/screens/chamasoft/dashboard.dart';
 import 'package:chamasoft/screens/chamasoft/transactions/wallet/review-withdrawal-requests.dart';
 import 'package:chamasoft/screens/chamasoft/transactions/wallet/withdrawal-purpose.dart';
 import 'package:chamasoft/utilities/svg-icons.dart';
 import 'package:chamasoft/widgets/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class ChamasoftTransactions extends StatefulWidget {
   ChamasoftTransactions({
@@ -46,20 +57,13 @@ class _ChamasoftTransactionsState extends State<ChamasoftTransactions> {
 
   @override
   Widget build(BuildContext context) {
-    final List<TransactionMenuSvg> list = [
-      TransactionMenuSvg("E-WALLET", customIcons['wallet']),
-      // TransactionMenuSvg("LOANS", customIcons['transaction']),
-      TransactionMenuSvg("RECORD PAYMENTS", customIcons['invoice']),
-      TransactionMenuSvg("RECORD EXPENDITURE", customIcons['expense']),
-      TransactionMenuSvg("INVOICING & TRANSFER", customIcons['transaction']),
-    ];
 
     List<Widget> eWalletOptions = [
       SizedBox(
         width: 16.0,
       ),
       Container(
-        width: 160.0,
+        width: 150.0,
         child: svgGridButton(
           context: context,
           icon: customIcons['wallet'],
@@ -78,7 +82,7 @@ class _ChamasoftTransactionsState extends State<ChamasoftTransactions> {
         width: 26.0,
       ),
       Container(
-        width: 160.0,
+        width: 150.0,
         child: svgGridButton(
           context: context,
           icon: customIcons['couple'],
@@ -98,6 +102,252 @@ class _ChamasoftTransactionsState extends State<ChamasoftTransactions> {
       ),
     ];
 
+    List<Widget> paymentsOptions = [
+      SizedBox(
+        width: 16.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['cash-register'],
+          title: 'CONTRIBUTIONS',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RecordContributionPayment(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['refund'],
+          title: 'FINES',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RecordFinePayment(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['cash-in-hand'],
+          title: 'INCOME',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RecordIncome(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['transaction'],
+          title: 'MISCELLANEOUS',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RecordMiscellaneousPayment(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['bank'],
+          title: 'BANK LOANS',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RecordBankLoan(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+    ];
+
+    List<Widget> expenditureOptions = [
+      SizedBox(
+        width: 16.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['invoice'],
+          title: 'EXPENSES',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RecordExpense(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['safe'],
+          title: 'BANK LOAN REPAYMENTS',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => BankLoanRepayment(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['money-bag'],
+          title: 'CONTRIBUTION REFUND',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => RecordContributionRefund(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+    ];
+
+    List<Widget> invoicingOptions = [
+      SizedBox(
+        width: 16.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['group'],
+          title: 'INVOICE MEMBERS',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => CreateInvoice(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['account'],
+          title: 'FINE MEMBER',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => FineMember(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['blockchain'],
+          title: 'CONTRIBUTION TRANSFER',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => ContributionTransfer(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+      Container(
+        width: 150.0,
+        child: svgGridButton(
+          context: context,
+          icon: customIcons['bank-cards'],
+          title: 'ACCOUNT TO ACCOUNT TRANSFER',
+          color: Colors.blue[400],
+          isHighlighted: false,
+          action: () => Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => AccountToAccountTransfer(),
+              settings: RouteSettings(arguments: 0)
+            )
+          ),
+          margin: 0
+        )
+      ),
+      SizedBox(
+        width: 26.0,
+      ),
+    ];
+
     return new WillPopScope(
       onWillPop: _onWillPop ,
       child: SafeArea(
@@ -106,7 +356,7 @@ class _ChamasoftTransactionsState extends State<ChamasoftTransactions> {
           child: Column(
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.fromLTRB(20.0, 10.0, 16.0, 10.0),
+                padding: EdgeInsets.fromLTRB(20.0, 0.0, 16.0, 0.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -119,12 +369,120 @@ class _ChamasoftTransactionsState extends State<ChamasoftTransactions> {
                         fontWeight: FontWeight.w600,
                       ),
                     ),
-                    // IconButton(
-                    //     icon: Icon(
-                    //       Feather.more_horizontal,
-                    //       color: Colors.blueGrey,
-                    //     ),
-                    //     onPressed: () {})
+                    IconButton(
+                        icon: Icon(
+                          Feather.more_horizontal,
+                          color: Colors.blueGrey,
+                        ),
+                        onPressed: () {})
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                child: Container(
+                  height: 180.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(top: 5.0, bottom: 10.0),
+                    physics: BouncingScrollPhysics(),
+                    children: eWalletOptions,
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 0.0, 16.0, 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Record Payments",
+                      style: TextStyle(
+                        color: Colors.blueGrey[400],
+                        fontFamily: 'SegoeUI',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Feather.more_horizontal,
+                          color: Colors.blueGrey,
+                        ),
+                        onPressed: () {})
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                child: Container(
+                  height: 180.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(top: 5.0, bottom: 10.0),
+                    physics: BouncingScrollPhysics(),
+                    children: paymentsOptions,
+                  ),
+                ),
+              ),
+              
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 0.0, 16.0, 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Record Expenditure",
+                      style: TextStyle(
+                        color: Colors.blueGrey[400],
+                        fontFamily: 'SegoeUI',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Feather.more_horizontal,
+                          color: Colors.blueGrey,
+                        ),
+                        onPressed: () {})
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+                child: Container(
+                  height: 180.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(top: 5.0, bottom: 10.0),
+                    physics: BouncingScrollPhysics(),
+                    children: expenditureOptions,
+                  ),
+                ),
+              ),
+              
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 0.0, 16.0, 0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Invoicing & Transfers",
+                      style: TextStyle(
+                        color: Colors.blueGrey[400],
+                        fontFamily: 'SegoeUI',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    IconButton(
+                        icon: Icon(
+                          Feather.more_horizontal,
+                          color: Colors.blueGrey,
+                        ),
+                        onPressed: () {})
                   ],
                 ),
               ),
@@ -136,11 +494,10 @@ class _ChamasoftTransactionsState extends State<ChamasoftTransactions> {
                     scrollDirection: Axis.horizontal,
                     padding: EdgeInsets.only(top: 5.0, bottom: 10.0),
                     physics: BouncingScrollPhysics(),
-                    children: eWalletOptions,
+                    children: invoicingOptions,
                   ),
                 ),
               ),
-              
             ]
           )
         )
