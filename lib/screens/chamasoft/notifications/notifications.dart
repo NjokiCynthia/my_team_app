8c684ae0571646405e9932b09c282f847ab08b9f
Samuel Wahome <samuel.wahome@digitalvision.co.ke>
2020-08-13 16:40:19 +0300

    Add Group Notifications Display

--- lib/screens/chamasoft/notifications/notifications.dart
+++ lib/screens/chamasoft/notifications/notifications.dart
@@ -48,6 +48,11 @@ class _ChamasoftNotificationsState extends State<ChamasoftNotifications> {
     try {
       await Provider.of<GroupProvider.Groups>(context, listen: false)
           .markAllNotificationsAsRead();
+      Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text(
+        "All notifications marked as read",
+      )));
+
       return true;
     } on CustomException catch (error) {
       StatusHandler().handleStatus(
@@ -70,153 +75,158 @@ class _ChamasoftNotificationsState extends State<ChamasoftNotifications> {
           elevation: 2.5,
           leadingIcon: LineAwesomeIcons.arrow_left),
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: (themeChangeProvider.darkTheme)
-                ? Colors.blueGrey[800]
-                : Color(0xffededfe),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  fit: FlexFit.tight,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      subtitle1(
-                          text: "Mark as Read",
-                          color: Theme.of(context).textSelectionHandleColor,
-                          textAlign: TextAlign.start),
-                      RichText(
-                        text: TextSpan(
-                            style: TextStyle(
-                                fontWeight: FontWeight.w300,
-                                fontSize: 13.0,
-                                color:
-                                    Theme.of(context).textSelectionHandleColor,
-                                fontFamily: 'SegoeUI'),
-                            children: [
-                              TextSpan(text: "Mark all notifications as "),
-                              TextSpan(
-                                  text: "read",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 13.0,
-                                      color: Theme.of(context)
-                                          .textSelectionHandleColor,
-                                      fontFamily: 'SegoeUI'))
-                            ]),
-                      ),
-                    ],
+      body: Builder(builder: (context) {
+        return Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(20.0),
+              color: (themeChangeProvider.darkTheme)
+                  ? Colors.blueGrey[800]
+                  : Color(0xffededfe),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        subtitle1(
+                            text: "Mark as Read",
+                            color: Theme.of(context).textSelectionHandleColor,
+                            textAlign: TextAlign.start),
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 13.0,
+                                  color: Theme.of(context)
+                                      .textSelectionHandleColor,
+                                  fontFamily: 'SegoeUI'),
+                              children: [
+                                TextSpan(text: "Mark all notifications as "),
+                                TextSpan(
+                                    text: "read",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 13.0,
+                                        color: Theme.of(context)
+                                            .textSelectionHandleColor,
+                                        fontFamily: 'SegoeUI'))
+                              ]),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                screenActionButton(
-                    icon: LineAwesomeIcons.check,
-                    textColor: Theme.of(context).textSelectionHandleColor,
-                    action: () async {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return Center(
-                              child: CircularProgressIndicator(),
-                            );
-                          });
-                      await markAllNotificationsAsRead(context);
-                      Navigator.pop(context);
-                    })
-              ],
+                  screenActionButton(
+                      icon: LineAwesomeIcons.check,
+                      textColor: Theme.of(context).textSelectionHandleColor,
+                      action: () async {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            });
+                        await markAllNotificationsAsRead(context);
+                        Navigator.pop(context);
+                      })
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: isFetched
-                ? Consumer<GroupProvider.Groups>(
-                    builder: (context, groupData, child) {
-                    return ListView.builder(
-                      shrinkWrap: true,
-                      itemBuilder: (context, index) {
-                        GroupProvider.Notification notification =
-                            groupData.notifications[index];
-                        return InkWell(
-                          onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) =>
-                                  NotificationDetails(
-                                      notification: notification),
+            Expanded(
+              child: isFetched
+                  ? Consumer<GroupProvider.Groups>(
+                      builder: (context, groupData, child) {
+                      return ListView.builder(
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          GroupProvider.Notification notification =
+                              groupData.notifications[index];
+                          return InkWell(
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    NotificationDetails(
+                                        notification: notification),
+                              ),
                             ),
-                          ),
-                          child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.all(SPACING_NORMAL),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: <Widget>[
-                                      Row(
-                                        children: <Widget>[
-                                          Icon(
-                                            Icons.notifications,
-                                            size: 24.0,
-                                            color: notification.isRead == "0"
-                                                ? Theme.of(context).hintColor
-                                                : Theme.of(context)
-                                                    .dividerColor,
-                                          ),
-                                          SizedBox(
-                                            width: 10,
-                                          ),
-                                          Expanded(
-                                            child: Text(
-                                              notification.message,
-                                              style: TextStyle(
-                                                  fontWeight:
-                                                      notification.isRead == "0"
-                                                          ? FontWeight.w700
-                                                          : FontWeight.w400,
-                                                  fontSize: 12.0,
-                                                  color: Theme.of(context)
-                                                      .textSelectionHandleColor,
-                                                  fontFamily: 'SegoeUI'),
-                                              textAlign: TextAlign.start,
+                            child: Container(
+                              child: Column(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.all(SPACING_NORMAL),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      children: <Widget>[
+                                        Row(
+                                          children: <Widget>[
+                                            Icon(
+                                              Icons.notifications,
+                                              size: 24.0,
+                                              color: notification.isRead == "0"
+                                                  ? Theme.of(context).hintColor
+                                                  : Theme.of(context)
+                                                      .dividerColor,
                                             ),
-                                          )
-                                        ],
-                                      ),
-                                      customTitle(
-                                          text: notification.timeAgo,
-                                          fontSize: 12.0,
-                                          fontWeight: notification.isRead == "0"
-                                              ? FontWeight.w700
-                                              : FontWeight.w400,
-                                          color: Theme.of(context)
-                                              .textSelectionHandleColor,
-                                          textAlign: TextAlign.end),
-                                    ],
+                                            SizedBox(
+                                              width: 10,
+                                            ),
+                                            Expanded(
+                                              child: Text(
+                                                notification.message,
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        notification.isRead ==
+                                                                "0"
+                                                            ? FontWeight.w700
+                                                            : FontWeight.w400,
+                                                    fontSize: 12.0,
+                                                    color: Theme.of(context)
+                                                        .textSelectionHandleColor,
+                                                    fontFamily: 'SegoeUI'),
+                                                textAlign: TextAlign.start,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        customTitle(
+                                            text: notification.timeAgo,
+                                            fontSize: 12.0,
+                                            fontWeight:
+                                                notification.isRead == "0"
+                                                    ? FontWeight.w700
+                                                    : FontWeight.w400,
+                                            color: Theme.of(context)
+                                                .textSelectionHandleColor,
+                                            textAlign: TextAlign.end),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                DashedDivider(
-                                  color: Theme.of(context).dividerColor,
-                                  thickness: 1.0,
-                                  height: 5.0,
-                                  width: 4.0,
-                                ),
-                              ],
+                                  DashedDivider(
+                                    color: Theme.of(context).dividerColor,
+                                    thickness: 1.0,
+                                    height: 5.0,
+                                    width: 4.0,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      itemCount: groupData.notifications.length,
-                    );
-                  })
-                : Center(
-                    child: CircularProgressIndicator(),
-                  ),
-          )
-        ],
-      ),
+                          );
+                        },
+                        itemCount: groupData.notifications.length,
+                      );
+                    })
+                  : Center(
+                      child: CircularProgressIndicator(),
+                    ),
+            )
+          ],
+        );
+      }),
     );
   }
 }
