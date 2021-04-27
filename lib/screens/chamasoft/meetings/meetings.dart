48595f8cec5aabf17450f83531e3112386be702c
Martin Nzuki <iamnzuki@gmail.com>
2021-04-27 10:43:02 +0300

    Meetings listing

--- lib/screens/chamasoft/meetings/meetings.dart
+++ lib/screens/chamasoft/meetings/meetings.dart
@@ -48,7 +48,30 @@ class _MeetingsState extends State<Meetings> {
 
     final List<dynamic> meetings = <dynamic>[
       {
-        'date': '',
+        'name': "Group's AGM meeting held at KNH",
+        'date': "12 April, 2021",
+        'members': {
+          'present': 10,
+          'absent': {
+            'with_apology': 2,
+            'without_apology': 1,
+          },
+        },
+      },
+      {
+        'name': "Loans critical discussions meeting",
+        'date': "21 April, 2021",
+        'members': {
+          'present': 8,
+          'absent': {
+            'with_apology': 3,
+            'without_apology': 2,
+          },
+        },
+      },
+      {
+        'name': "Group's AGM meeting held at KNH",
+        'date': "12 April, 2021",
         'members': {
           'present': 10,
           'absent': {
@@ -81,11 +104,9 @@ class _MeetingsState extends State<Meetings> {
               ),
             ),
           ]),
-      body: Builder(builder: (BuildContext context) {
-        return SingleChildScrollView(
-          controller: _scrollController,
-          padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 40.0),
-          child: Column(
+      body: Builder(
+        builder: (BuildContext context) {
+          return Column(
             children: <Widget>[
               Container(
                 padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
@@ -99,7 +120,7 @@ class _MeetingsState extends State<Meetings> {
                       Icons.lightbulb_outline,
                       color: Theme.of(context).textSelectionHandleColor,
                       size: 24.0,
-                      semanticLabel: 'Text to announce in accessibility modes',
+                      semanticLabel: 'You should know...',
                     ),
                     SizedBox(
                       width: 10,
@@ -125,58 +146,104 @@ class _MeetingsState extends State<Meetings> {
                   ],
                 ),
               ),
-              Container(
-                padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    subtitle2(
-                      text: "12 April, 2021",
-                      color: Theme.of(context).textSelectionHandleColor,
-                      textAlign: TextAlign.start,
-                    ),
-                    subtitle1(
-                      text: "Group's AGM meeting held at KNH",
-                      color: Theme.of(context).textSelectionHandleColor,
-                      textAlign: TextAlign.start,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          "Members present ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 13.0,
-                            color: Theme.of(context).textSelectionHandleColor,
-                            fontFamily: 'SegoeUI',
-                          ),
-                          textAlign: TextAlign.start,
+              SingleChildScrollView(
+                controller: _scrollController,
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 40.0),
+                child: meetings.length > 0
+                    ? ListView.separated(
+                        controller: _scrollController,
+                        shrinkWrap: true,
+                        separatorBuilder: (context, index) => Divider(
+                          color: Theme.of(context)
+                              .textSelectionHandleColor
+                              .withOpacity(0.5),
+                          height: 0.0,
                         ),
-                        Text(
-                          "12",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 13.0,
-                            color: Theme.of(context).textSelectionHandleColor,
-                            fontFamily: 'SegoeUI',
-                          ),
-                          textAlign: TextAlign.start,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                        itemCount: meetings.length,
+                        itemBuilder: (context, index) {
+                          return InkWell(
+                            onTap: () => {},
+                            child: Container(
+                              padding: EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0),
+                              width: double.infinity,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 0.0),
+                                    child: IconButton(
+                                      icon: Icon(
+                                        Icons.group,
+                                        color: Theme.of(context)
+                                            .textSelectionHandleColor,
+                                      ),
+                                      onPressed: () => {
+                                        // Add new meeting
+                                      },
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      subtitle2(
+                                        text: meetings[index]['date'],
+                                        color: Theme.of(context)
+                                            .textSelectionHandleColor,
+                                        textAlign: TextAlign.start,
+                                      ),
+                                      subtitle1(
+                                        text: meetings[index]['name'],
+                                        color: Theme.of(context)
+                                            .textSelectionHandleColor,
+                                        textAlign: TextAlign.start,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            "Members present ",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w300,
+                                              fontSize: 13.0,
+                                              color: Theme.of(context)
+                                                  .textSelectionHandleColor,
+                                              fontFamily: 'SegoeUI',
+                                            ),
+                                            textAlign: TextAlign.start,
+                                          ),
+                                          Text(
+                                            meetings[index]['members']
+                                                    ['present']
+                                                .toString(),
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 13.0,
+                                              color: Theme.of(context)
+                                                  .textSelectionHandleColor,
+                                              fontFamily: 'SegoeUI',
+                                            ),
+                                            textAlign: TextAlign.start,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    : emptyList(
+                        color: Colors.blue[400],
+                        iconData: LineAwesomeIcons.file_text,
+                        text: "There are no meetings to show",
+                      ),
               ),
-              // emptyList(
-              //   color: Colors.blue[400],
-              //   iconData: LineAwesomeIcons.file_text,
-              //   text: "There are no meetings to show",
-              // ),
             ],
-          ),
-        );
-      }),
+          );
+        },
+      ),
     );
   }
 }
