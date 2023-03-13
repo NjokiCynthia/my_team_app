97fe904f8717718ba1baeb34cfb8db43d8614bd3
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2023-03-13 21:25:51 +0300

    solve WebView error

--- lib/screens/pdfAPI.dart
+++ lib/screens/pdfAPI.dart
@@ -12,10 +12,11 @@ import 'package:chamasoft/screens/chamasoft/models/statement-row.dart';
 import 'package:chamasoft/screens/chamasoft/models/summary-row.dart';
 import 'package:chamasoft/screens/chamasoft/models/transaction-statement-model.dart';
 import 'package:flutter/services.dart';
-import 'package:open_file/open_file.dart';
+//import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
+import 'package:open_file/open_file.dart';
 
 class PdfApi {
   static Future<File> generateMemberLoanStatementPdf(
